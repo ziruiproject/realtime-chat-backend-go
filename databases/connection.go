ed2b11d5e8b63be5a1f80e749f58dbf29d8aca73
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-17 14:35:12 +0700

    making connection for Psql

--- databases/connection.go
+++ databases/connection.go
@@ -1,5 +1,40 @@
 package databases
 
-func connection() {
-	
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/helper"
+)
+
+func Connection() {
+	helper.LoadEnv()
+
+	dbData := map[string]string{
+		"host": helper.GetEnv("HOST"),
+		"port": helper.GetEnv("PORT"),
+		"user": helper.GetEnv("DB_USERNAME"),
+		"name": helper.GetEnv("DB_NAME"),
+		"pass": helper.GetEnv("DB_PASSWORD"),
+	}
+
+	dbconn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbData["host"], dbData["port"], dbData["user"], dbData["pass"], dbData["name"],
+	)
+
+	db, err := sql.Open("postgres", dbconn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to PostreSQL database")
 }
