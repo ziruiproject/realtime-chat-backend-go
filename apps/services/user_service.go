890c227a829f35e5ef22c3a19584a4f020f6ba17
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-18 20:59:54 +0700

    minor type changes

--- apps/services/user_service.go
+++ apps/services/user_service.go
@@ -2,9 +2,10 @@ package services
 
 import (
 	"context"
+
 	"github.com/ziruiproject/realtime-chat-backend-go/apps/web"
 )
 
 type UserService interface {
-	GetAll(ctx context.Context) web.UserResponse
+	GetAll(ctx context.Context) []web.UserResponse
 }
