c2993dcc6b1d800e07311b92e249c3cd18c962f0
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-18 14:47:14 +0700

    Adding transactional connection

--- databases/connection.go
+++ databases/connection.go
@@ -5,19 +5,35 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
+
 	_ "github.com/lib/pq"
-	"github.com/ziruiproject/realtime-chat-backend-go/apps/helper"
+	_ "github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
 )
 
-func Connection() {
-	helper.LoadEnv()
+type Database struct {
+	Db *sql.DB
+	Tx *sql.Tx
+}
 
+func ConstrDatabase() *Database {
+	var db *sql.DB = connection()
+	var tx *sql.Tx = txConnection(db)
+
+	return &Database{
+		Db: db,
+		Tx: tx,
+	}
+}
+
+func connection() *sql.DB {
+	helpers.LoadEnv()
 	dbData := map[string]string{
-		"host": helper.GetEnv("HOST"),
-		"port": helper.GetEnv("PORT"),
-		"user": helper.GetEnv("DB_USERNAME"),
-		"name": helper.GetEnv("DB_NAME"),
-		"pass": helper.GetEnv("DB_PASSWORD"),
+		"host": helpers.GetEnv("HOST"),
+		"port": helpers.GetEnv("PORT"),
+		"user": helpers.GetEnv("DB_USERNAME"),
+		"name": helpers.GetEnv("DB_NAME"),
+		"pass": helpers.GetEnv("DB_PASSWORD"),
 	}
 
 	dbconn := fmt.Sprintf(
@@ -26,15 +42,30 @@ func Connection() {
 	)
 
 	db, err := sql.Open("postgres", dbconn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	helpers.ErrorWithLog("Failed to connecting to database", err)
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		helpers.ErrorWithLog("Failed to close db connection", err)
+	}(db)
 
 	log.Println("Connected to PostreSQL database")
+
+	return db
+}
+
+func txConnection(db *sql.DB) *sql.Tx {
+
+	tx, err := db.Begin()
+	helpers.ErrorWithLog("Failed to start transaction:", err)
+
+	defer func(tx *sql.Tx) {
+		err := tx.Rollback()
+		helpers.ErrorWithLog("Failed to rollback transaction:", err)
+	}(tx)
+
+	err = tx.Commit()
+	helpers.ErrorWithLog("Failed to commit transaction:", err)
+
+	return tx
 }
