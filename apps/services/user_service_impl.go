6f5d10586375988c6ace2d5a050e4a019281a834
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-18 21:00:28 +0700

    adding contructor function

--- apps/services/user_service_impl.go
+++ apps/services/user_service_impl.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"database/sql"
+	"log"
 
 	"github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
 
@@ -16,9 +17,18 @@ type UserServiceImpl struct {
 	DB             *sql.DB
 }
 
+func NewUserService(userRepository repositories.UserRepository, db *sql.DB) UserService {
+	return &UserServiceImpl{
+		UserRepository: userRepository,
+		DB:             db,
+	}
+}
+
 func (service *UserServiceImpl) GetAll(ctx context.Context) []web.UserResponse {
+	log.Println(service.DB)
+	log.Println("Masuk Service")
 	tx, err := service.DB.Begin()
-	helpers.ErrorWithLog("Failed to make the transaction", err)
+	helpers.ErrorWithLog("Failed to make the transaction: ", err)
 	defer helpers.CommitOrRollback(tx)
 
 	users := service.UserRepository.GetAll(ctx, tx)
