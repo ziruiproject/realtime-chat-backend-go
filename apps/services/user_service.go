cd5548fda8d9aaf7663bcfcb3ce84f77e09a975f
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-18 15:59:20 +0700

    adding user service interface

---
+++ apps/services/user_service.go
@@ -0,0 +1,10 @@
+package services
+
+import (
+	"context"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/web"
+)
+
+type UserService interface {
+	GetAll(ctx context.Context) web.UserResponse
+}
