1c6b95e64a71c119ccfbfb18e35e1a9a3e83403d
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-18 21:03:22 +0700

    removing transactional connection

--- databases/connection.go
+++ databases/connection.go
@@ -11,22 +11,7 @@ import (
 	_ "github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
 )
 
-type Database struct {
-	Db *sql.DB
-	Tx *sql.Tx
-}
-
-func ConstrDatabase() *Database {
-	var db *sql.DB = connection()
-	var tx *sql.Tx = txConnection(db)
-
-	return &Database{
-		Db: db,
-		Tx: tx,
-	}
-}
-
-func connection() *sql.DB {
+func Connection() (*sql.DB, error) {
 	helpers.LoadEnv()
 	dbData := map[string]string{
 		"host": helpers.GetEnv("HOST"),
@@ -43,29 +28,8 @@ func connection() *sql.DB {
 
 	db, err := sql.Open("postgres", dbconn)
 	helpers.ErrorWithLog("Failed to connecting to database", err)
-
-	defer func(db *sql.DB) {
-		err := db.Close()
-		helpers.ErrorWithLog("Failed to close db connection", err)
-	}(db)
-
+	
 	log.Println("Connected to PostreSQL database")
 
-	return db
-}
-
-func txConnection(db *sql.DB) *sql.Tx {
-
-	tx, err := db.Begin()
-	helpers.ErrorWithLog("Failed to start transaction:", err)
-
-	defer func(tx *sql.Tx) {
-		err := tx.Rollback()
-		helpers.ErrorWithLog("Failed to rollback transaction:", err)
-	}(tx)
-
-	err = tx.Commit()
-	helpers.ErrorWithLog("Failed to commit transaction:", err)
-
-	return tx
+	return db, nil
 }
