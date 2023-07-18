df3a3c04256e3464ce4b340cde0f37712784b859
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-18 20:57:43 +0700

    adding controller loader

--- apps/main.go
+++ apps/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/controllers"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
+	"github.com/ziruiproject/realtime-chat-backend-go/databases"
 	"github.com/ziruiproject/realtime-chat-backend-go/routes"
 )
 
 func main() {
 	app := fiber.New()
-	routes.SetupRoute(app)
+	db, err := databases.Connection()
+	helpers.ErrorWithLog("Error when connecting to db: ", err)
+	
+	loadController := controllers.SetupController(db)
+
+	routes.SetupRoute(app, loadController)
 	app.Listen(":3000")
 }
