bb870e3a6ad53e43b8e33dbe2a0807b82c021eee
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-18 21:02:10 +0700

    adding json format

--- apps/web/user_response.go
+++ apps/web/user_response.go
@@ -1,10 +1,10 @@
 package web
 
 type UserResponse struct {
-	Id        string
-	Name      string
-	Email     string
-	Profile   string
-	CreatedAt int
-	UpdatedAt int
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Profile   string `json:"profile"`
+	CreatedAt int    `json:"created_at"`
+	UpdatedAt int    `json:"updated_at"`
 }
