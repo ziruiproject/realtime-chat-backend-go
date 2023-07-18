23afac52ac94c7301894c422eaebc3bbbec58445
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-18 21:04:24 +0700

    fixing route and controller

--- routes/api.go
+++ routes/api.go
@@ -1,10 +1,15 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
 
-func SetupRoute(app *fiber.App) {
-	app.Get("/", homeHandler)
-}
-func homeHandler(c *fiber.Ctx) error {
-	return c.SendString("Hello World")
+	"github.com/gofiber/fiber/v2"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/controllers"
+)
+
+func SetupRoute(app *fiber.App, controller *controllers.Controllers) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		log.Println("Masuk Route")
+		return controller.UserController.GetAll(c)
+	})
 }
