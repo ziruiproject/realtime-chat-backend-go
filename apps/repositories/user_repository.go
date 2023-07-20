959bc3e5382df6beb86e8c75adfbd742b238c53c
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-20 20:26:59 +0700

    fixing data types

--- apps/repositories/user_repository.go
+++ apps/repositories/user_repository.go
@@ -8,9 +8,9 @@ import (
 )
 
 type UserRepository interface {
-//	Save(ctx context.Context, tx *sql.Tx, user *models.User) models.User
-//	Update(ctx context.Context, tx *sql.Tx, user *models.User) models.User
-//	Delete(ctx context.Context, tx *sql.Tx, user *models.User)
-//	GetById(ctx context.Context, tx *sql.Tx, id string) models.User
+	Save(ctx context.Context, tx *sql.Tx, user models.User) models.User
+	Update(ctx context.Context, tx *sql.Tx, user models.User) models.User
+	Delete(ctx context.Context, tx *sql.Tx, user models.User)
+	GetById(ctx context.Context, tx *sql.Tx, id string) (models.User, error)
 	GetAll(ctx context.Context, tx *sql.Tx) []models.User
 }
