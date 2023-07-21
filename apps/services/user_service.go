ed386c48d7a9f6325d97ffe4cb960c1993e58805
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-21 17:18:22 +0700

    added CUD signatures

--- apps/services/user_service.go
+++ apps/services/user_service.go
@@ -3,9 +3,15 @@ package services
 import (
 	"context"
 
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/models"
+
 	"github.com/ziruiproject/realtime-chat-backend-go/apps/web"
 )
 
 type UserService interface {
 	GetAll(ctx context.Context) []web.UserResponse
+	GetById(ctx context.Context, userId string) web.UserResponse
+	Create(ctx context.Context, user models.User) web.UserResponse
+	Update(ctx context.Context, user models.User) web.UserResponse
+	Delete(ctx context.Context, userId string)
 }
