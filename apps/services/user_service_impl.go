073104f2cd73489da3f7697bf15b9fe72c0b7325
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-21 21:25:19 +0700

    building getbyid service

--- apps/services/user_service_impl.go
+++ apps/services/user_service_impl.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"time"
 
-	"github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
-
-	"github.com/ziruiproject/realtime-chat-backend-go/apps/web"
+	"github.com/google/uuid"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/models"
 
 	"github.com/ziruiproject/realtime-chat-backend-go/apps/repositories"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/web/requests"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/web/responses"
+
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
 )
 
 type UserServiceImpl struct {
@@ -24,7 +28,7 @@ func NewUserService(userRepository repositories.UserRepository, db *sql.DB) User
 	}
 }
 
-func (service *UserServiceImpl) GetAll(ctx context.Context) []web.UserResponse {
+func (service *UserServiceImpl) GetAll(ctx context.Context) []responses.UserResponse {
 	log.Println("Masuk Service")
 	tx, err := service.DB.Begin()
 	helpers.ErrorWithLog("Failed to make the transaction: ", err)
@@ -32,10 +36,51 @@ func (service *UserServiceImpl) GetAll(ctx context.Context) []web.UserResponse {
 
 	users := service.UserRepository.GetAll(ctx, tx)
 
-	var userResponse []web.UserResponse
+	var userResponse []responses.UserResponse
 	for _, user := range users {
 		userResponse = append(userResponse, helpers.ToUserResponse(user))
 	}
 
 	return userResponse
+}
+
+func (service *UserServiceImpl) GetById(ctx context.Context, userId string) responses.UserResponse {
+	tx, err := service.DB.Begin()
+	helpers.ErrorWithLog("Failed to make transaction", err)
+	defer helpers.CommitOrRollback(tx)
+
+	user, err := service.UserRepository.GetById(ctx, tx, userId)
+	helpers.ErrorWithLog("Failed retrivign user", err)
+
+	return helpers.ToUserResponse(user)
+}
+
+func (service *UserServiceImpl) Create(ctx context.Context, request requests.UserCreateRequset) responses.UserResponse {
+	tx, err := service.DB.Begin()
+	helpers.ErrorWithLog("Failed to make transaction", err)
+	defer helpers.CommitOrRollback(tx)
+
+	user := models.User{
+		Id:        uuid.New(),
+		Email:     request.Email,
+		Name:      request.Name,
+		Password:  request.Password,
+		CreatedAt: time.Now().Unix(),
+		UpdatedAt: time.Now().Unix(),
+	}
+
+	user = service.UserRepository.Save(ctx, tx, user)
+
+	return helpers.ToUserResponse(user)
+}
+
+func (service *UserServiceImpl) Update(ctx context.Context, request requests.UserUpdateRequest, userId uuid.UUID) responses.UserResponse {
+	tx, err := service.DB.Begin()
+	helpers.ErrorWithLog("Failed to make transaction", err)
+	defer helpers.CommitOrRollback(tx)
+
+	user, err := service.UserRepository.GetById(ctx, tx, userId)
+	helpers.ErrorWithLog("Failed retriving user", err)
+
+	user= 
 }
