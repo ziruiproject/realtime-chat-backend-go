b60a612336f9ff3b9b87c8641eec0da1ae542408
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-21 21:26:27 +0700

    fixing data type

--- apps/repositories/user_repository.go
+++ apps/repositories/user_repository.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"database/sql"
 
+	"github.com/google/uuid"
+
 	"github.com/ziruiproject/realtime-chat-backend-go/apps/models"
 )
 
 type UserRepository interface {
 	Save(ctx context.Context, tx *sql.Tx, user models.User) models.User
 	Update(ctx context.Context, tx *sql.Tx, user models.User) models.User
-	Delete(ctx context.Context, tx *sql.Tx, id string)
-	GetById(ctx context.Context, tx *sql.Tx, id string) (models.User, error)
+	Delete(ctx context.Context, tx *sql.Tx, id uuid.UUID)
+	GetById(ctx context.Context, tx *sql.Tx, id uuid.UUID) (models.User, error)
 	GetAll(ctx context.Context, tx *sql.Tx) []models.User
 }
