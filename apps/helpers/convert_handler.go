91fe1dbeea153aec98db7b9da2042a447fe2c2a1
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-21 21:26:55 +0700

    fixing package name

--- apps/helpers/convert_handler.go
+++ apps/helpers/convert_handler.go
@@ -2,14 +2,16 @@ package helpers
 
 import (
 	"github.com/ziruiproject/realtime-chat-backend-go/apps/models"
-	"github.com/ziruiproject/realtime-chat-backend-go/apps/web"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/web/responses"
 )
 
-func ToUserResponse(user models.User) web.UserResponse{
-	return web.UserResponse{
-		Id: user.Id,
-		Name: user.Name,
-		Email: user.Email,
+func ToUserResponse(user models.User) responses.UserResponse {
+	return responses.UserResponse{
+		Id:      user.Id,
+		Name:    user.Name,
+		Email:   user.Email,
 		Profile: user.Profile,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
 	}
 }
