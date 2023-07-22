b05dd6532e1fec11fb78669f6c41fe6eef1821a1
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-22 14:55:00 +0700

    adding getbyid controller

--- apps/controllers/user_controller_impl.go
+++ apps/controllers/user_controller_impl.go
@@ -1,8 +1,15 @@
 package controllers
 
 import (
-	"github.com/ziruiproject/realtime-chat-backend-go/apps/web/responses"
 	"log"
+
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/web/requests"
+
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
+
+	"github.com/google/uuid"
+
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/web/responses"
 
 	"github.com/gofiber/fiber/v2"
 
@@ -29,4 +36,18 @@ func (controller *UserControllerImpl) GetAll(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(apiResponse)
+}
+
+func (controller *UserControllerImpl) GetById(c *fiber.Ctx) error {
+	userId, err := uuid.Parse(c.Params("id"))
+	helpers.ErrorWithLog("Failed parsing id", err)
+
+	userResponse := controller.UserService.GetById(c.Context(), userId)
+	apiReesponse := responses.ApiResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   userResponse,
+		}
+
+		return c.JSON(apiReesponse)
 }
