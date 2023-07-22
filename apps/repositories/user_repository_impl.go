cdc02f68c761be5cf7fb79787808b7d601b4cd45
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-22 14:53:17 +0700

    fixing queries

--- apps/repositories/user_repository_impl.go
+++ apps/repositories/user_repository_impl.go
@@ -40,7 +40,7 @@ func (repository *UserRepositoryImpl) GetAll(ctx context.Context, tx *sql.Tx) []
 }
 
 func (repository *UserRepositoryImpl) GetById(ctx context.Context, tx *sql.Tx, id uuid.UUID) (models.User, error) {
-	var SQL string = `SELECT id, name, email, profile_img FROM users WHERE id = ?`
+	var SQL string = "SELECT id, name, email, profile_img, created_at, updated_at FROM users WHERE id = $1"
 	rows, err := tx.QueryContext(ctx, SQL, id)
 	helpers.ErrorWithLog("Failed retriving user", err)
 	defer helpers.ErrorCloseRowsDefer(rows)
@@ -50,14 +50,20 @@ func (repository *UserRepositoryImpl) GetById(ctx context.Context, tx *sql.Tx, i
 		return user, errors.New("user not found")
 	}
 
-	err = rows.Scan(&user.Id, &user.Name, &user.Email, &user.Profile)
+	err = rows.Scan(&user.Id, &user.Name, &user.Email, &user.Profile, &user.CreatedAt, &user.UpdatedAt)
 	helpers.ErrorWithLog("Failed scanning query", err)
 	return user, nil
 }
 
 func (repository *UserRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, user models.User) models.User {
-	var SQL string = `INSERT INTO users(id, name, email, password, profile_img) values (?, ?, ?, ?, ?)`
-	_, err := tx.ExecContext(ctx, SQL, user.Id, user.Name, user.Email, user.Password, user.Profile)
+	log.Println("id", user.Id)
+	log.Println("name", user.Name)
+	log.Println("email", user.Email)
+	log.Println("pass", user.Password)
+	log.Println("create", user.CreatedAt)
+	log.Println("update", user.UpdatedAt)
+	var SQL string = "INSERT INTO users(id,name,email,password,created_at,updated_at) values($1, $2, $3, $4, $5, $6)"
+	_, err := tx.ExecContext(ctx, SQL, user.Id, user.Name, user.Email, user.Password, user.CreatedAt, user.UpdatedAt)
 	helpers.ErrorWithLog("Failed creating user", err)
 
 	return user
@@ -65,20 +71,19 @@ func (repository *UserRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, user
 
 func (repository *UserRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, user models.User) models.User {
 	var SQL string = `UPDATE users SET
-		email = ?,
-		password = ?,
-		profile_img = ?,
-		created_at = ?,
-		updated_at = ?
-		WHERE id::text = ?;`
-	_, err := tx.QueryContext(ctx, SQL, user.Email, user.Password, user.Profile, user.CreatedAt, user.UpdatedAt, user.Id)
+		email = $1,
+		name = $2,
+		created_at = $3,
+		updated_at = $4
+		WHERE id::text = $5;`
+	_, err := tx.QueryContext(ctx, SQL, user.Email, user.Name, user.CreatedAt, user.UpdatedAt, user.Id)
 	helpers.ErrorWithLog("Failed updating user", err)
 
 	return user
 }
 
 func (repository UserRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, id uuid.UUID) {
-	var SQL string = `DELETE FROM users WHERE id = ?`
+	var SQL string = `DELETE FROM users WHERE id = $1`
 	_, err := tx.ExecContext(ctx, SQL, id)
 	helpers.ErrorWithLog("Failed deleting user", err)
 }
