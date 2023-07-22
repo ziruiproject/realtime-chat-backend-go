0fa6563e9c372a257fec9526737ea217ec428f3c
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-07-22 14:51:17 +0700

    adding api routes

--- routes/api.go
+++ routes/api.go
@@ -1,15 +1,24 @@
 package routes
 
 import (
-	"log"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/ziruiproject/realtime-chat-backend-go/apps/controllers"
 )
 
 func SetupRoute(app *fiber.App, controller *controllers.Controllers) {
-	app.Get("/", func(c *fiber.Ctx) error {
-		log.Println("Masuk Route")
+	app.Get("/users", func(c *fiber.Ctx) error {
 		return controller.UserController.GetAll(c)
+	})
+	app.Get("/users/:id", func(c *fiber.Ctx) error {
+		return controller.UserController.GetById(c)
+	})
+	app.Post("/users", func(c *fiber.Ctx) error {
+		return controller.UserController.Create(c)
+	})
+	app.Patch("/users/:id", func(c *fiber.Ctx) error {
+		return controller.UserController.Update(c)
+	})
+	app.Delete("/users/:id", func(c *fiber.Ctx) error {
+		return controller.UserController.Delete(c)
 	})
 }
