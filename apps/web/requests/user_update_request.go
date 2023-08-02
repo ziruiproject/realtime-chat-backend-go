758a79983d2d4aadfc4a8d2a11a16561b922aa45
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-08-02 21:12:19 +0700

    merge to user_request

--- apps/web/requests/user_update_request.go
+++
@@ -1,8 +0,0 @@
-package requests
-
-type UserUpdateRequest struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	//	Username string `json:"username"`
-//	Profile string `json:"profile"`
-}
