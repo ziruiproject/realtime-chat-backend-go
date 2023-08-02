0a1bd655b366339b121b7c00053dcf417e249ed4
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-08-02 21:10:16 +0700

    fixing type

--- apps/services/user_service.go
+++ apps/services/user_service.go
@@ -13,7 +13,7 @@ import (
 type UserService interface {
 	GetAll(ctx context.Context) []responses.UserResponse
 	GetById(ctx context.Context, userId uuid.UUID) responses.UserResponse
-	Create(ctx context.Context, request requests.UserCreateRequset) responses.UserResponse
+	Create(ctx context.Context, request requests.UserCreateRequest) responses.UserResponse
 	Update(ctx context.Context, request requests.UserUpdateRequest, userId uuid.UUID) responses.UserResponse
 	Delete(ctx context.Context, userId uuid.UUID)
 }
