c6785207b4ba18a4336eefa9b83aa0255324a831
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-08-02 21:10:00 +0700

    implementing jwt at login

---
+++ apps/services/auth_service_impl.go
@@ -0,0 +1,55 @@
+package services
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/web/responses"
+
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
+
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/web/requests"
+
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/repositories"
+)
+
+type AuthServiceImpl struct {
+	UserRepository repositories.UserRepository
+	DB             *sql.DB
+}
+
+func (service *AuthServiceImpl) Login(ctx context.Context, request requests.UserLoginRequest) responses.AuthResponse {
+	helpers.LoadEnv()
+	tx, err := service.DB.Begin()
+	helpers.ErrorWithLog("Failed to make the transaction: ", err)
+	defer helpers.CommitOrRollback(tx)
+
+	user, err := service.UserRepository.GetCredentials(ctx, tx, request.Email)
+	helpers.ErrorWithLog("Failed retriving user", err)
+
+	if !helpers.VerifyPassword(user.Password, request.Password) {
+		return responses.AuthResponse{
+			UserId:  nil,
+			Token:   "",
+			Success: false,
+		}
+	}
+
+	// IMPLEMENT JWT
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["user_id"] = user.Id
+	claims["exp"] = time.Now().Add(time.Hour * 48).Unix()
+
+	tokenString, err := token.SignedString(helpers.GetEnv("JWT_SECRET"))
+	helpers.ErrorWithLog("Failed generating token", err)
+
+	return responses.AuthResponse{
+		UserId:  &user.Id,
+		Token:   tokenString,
+		Success: true,
+	}
+}
