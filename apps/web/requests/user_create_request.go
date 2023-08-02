6e99e0b692adbe477e438a4e3eb1a7cf8b4704ce
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-08-02 21:11:08 +0700

    renaming file

--- apps/web/requests/user_create_request.go
+++
@@ -1,8 +0,0 @@
-package requests
-
-type UserCreateRequset struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-//	Username string `json:"username"`
-	Password string `json:"password"`
-}
