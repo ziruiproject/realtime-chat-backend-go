c2481b878f22f3462ef36e4ae12d7f69b394a567
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-09-26 18:39:50 +0700

    switch to env loader

--- apps/main.go
+++ apps/main.go
@@ -5,11 +5,15 @@ import (
 	"github.com/ziruiproject/realtime-chat-backend-go/apps/controllers"
 	"github.com/ziruiproject/realtime-chat-backend-go/apps/helpers"
 	"github.com/ziruiproject/realtime-chat-backend-go/databases"
+	//	"github.com/ziruiproject/realtime-chat-backend-go/middlewares"
 	"github.com/ziruiproject/realtime-chat-backend-go/routes"
 )
 
 func main() {
 	app := fiber.New()
+
+	helpers.LoadEnv()
+
 	db, err := databases.Connection()
 	helpers.ErrorWithLog("Error when connecting to db: ", err)
 
@@ -19,6 +23,8 @@ func main() {
 		c.Set("Content-Type", "application/json")
 		return c.Next()
 	})
+
+//	app.Use("/users", middlewares.AuthMiddleware)
 
 	routes.SetupRoute(app, loadController)
 	app.Listen(":3000")
