efa12e6eb3df66c1179d1244141fe953b97a7229
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-09-26 18:39:18 +0700

    make env loader

--- apps/helpers/env_handler.go
+++ apps/helpers/env_handler.go
@@ -1,23 +1,34 @@
 package helpers
 
 import (
+	"github.com/gofor-little/env"
+	configs "github.com/ziruiproject/realtime-chat-backend-go/apps/configuration"
 	"log"
-	"os"
-
-	"github.com/joho/godotenv"
+	//	"github.com/joho/godotenv"
+	//	_ "github.com/joho/godotenv/autoload"
+	//	"path/filepath"
 )
 
 func LoadEnv() {
-	err := godotenv.Load("../.env")
+	err := env.Load("../.env")
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	 configs.EnvConfigs = &configs.Config{
+		DBUsername: GetEnv("DB_USERNAME"),
+		DBPassword: GetEnv("DB_PASSWORD"),
+		DBName: GetEnv("DB_NAME"),
+		DBPort: GetEnv("PORT"),
+		DBHost: GetEnv("HOST"),
+		SecretToken: GetEnv("JWT_SECRET"),
 	}
 }
 
 func GetEnv(name string) string {
-	value := os.Getenv(name)
-	if value == "" {
-		log.Fatal("Env not found")
+	value, err := env.MustGet(name)
+	if err != nil {
+		log.Fatal(err)
 	}
 	return value
 }
