bfebe8b49247891cc5b8456219525fa1c1c626df
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-09-26 18:38:52 +0700

    implement jwt

--- apps/controllers/auth_controller_impl.go
+++ apps/controllers/auth_controller_impl.go
@@ -1,20 +1,20 @@
 package controllers
-
-import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/ziruiproject/realtime-chat-backend-go/apps/services"
-)
-
-type AuthControllerImpl struct {
-	AuthService services.AuthService
-}
-
-func NewAuthController(authService services.AuthService) AuthController {
-	return &AuthControllerImpl{
-		AuthService: authService,
-	}
-}
-
-func (controller *AuthControllerImpl) Login(c *fiber.Ctx) error {
-	
-}
+//
+//import (
+//	"github.com/gofiber/fiber/v2"
+//	"github.com/ziruiproject/realtime-chat-backend-go/apps/services"
+//)
+//
+//type AuthControllerImpl struct {
+//	AuthService services.AuthService
+//}
+//
+//func NewAuthController(authService services.AuthService) AuthController {
+//	return &AuthControllerImpl{
+//		AuthService: authService,
+//	}
+//}
+//
+//func (controller *AuthControllerImpl) Login(c *fiber.Ctx) error {
+//
+//}
