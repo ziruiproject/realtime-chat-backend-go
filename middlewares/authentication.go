9295e84115477589b7c80706b348f2ae7e823cf5
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-09-28 16:42:30 +0700

    implement jwt middleware

--- middlewares/authentication.go
+++ middlewares/authentication.go
@@ -3,8 +3,45 @@ package middlewares
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
+	configs "github.com/ziruiproject/realtime-chat-backend-go/apps/configuration"
+	"github.com/ziruiproject/realtime-chat-backend-go/apps/web/responses"
 )
 
-func AuthMiddleware(secret string) fiber.Handler {
+func AuthMiddleware(c *fiber.Ctx) error {
+//	GET AUTH HEADER
+	authHeader := c.Get("X-AUTH-HEADER")
+
+	if authHeader == "" {
+		return c.Status(401).JSON(responses.ApiResponse{
+			Code: 401,
+			Status: "Unauthorized",
+			Data: "",
+		})
+	}
+
+//	PARSE TOKEN
+	token, err := jwt.Parse(authHeader, func(token *jwt.Token) (interface{}, error) {
+		return configs.EnvConfigs.SecretToken, nil
+	})
+
+//	CHECK IF ERROR PARSING TOKEN
+	if err != nil {
+		return c.Status(401).JSON(responses.ApiResponse{
+			Code: 401,
+			Status: "Unauthorized",
+			Data: "",
+		})
+	}
+
+//	CHECK IF THE TOKEN VALID
+	if !token.Valid {
+		return c.Status(401).JSON(responses.ApiResponse{
+			Code: 401,
+			Status: "Unauthorized",
+			Data: "",
+		})
+	}
+
+	return c.Next()
 	
 }
