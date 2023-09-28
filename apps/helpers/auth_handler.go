400e1e6457b1cbc7b03ea8a8137faf7a14acdb7c
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-09-28 21:22:23 +0700

    minor changes

--- apps/helpers/auth_handler.go
+++ apps/helpers/auth_handler.go
@@ -1,12 +1,16 @@
 package helpers
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"golang.org/x/crypto/bcrypt"
+)
 
 func VerifyPassword(userPassword string, requestPassword string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(userPassword), []byte(requestPassword))
+	
 	return err == nil
 }
 
+// HashPassword
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	if err != nil {
